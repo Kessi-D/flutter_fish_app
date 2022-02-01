729479fbd6df2eb713cfc2205fcac8f7866198e2
Kessi <kessidarlington@gmail.com>
2022-02-01 20:54:43 +0000

    add landscape responsivesness

--- lib/home.dart
+++ lib/home.dart
@@ -18,99 +18,112 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("assets/fishing.jpg"),
-          Container(
-            transform: Matrix4.translationValues(0.0, -12.0, 0.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/fishing.jpg"),
+
+            // Expanded(child: Image.asset("assets/fishing.jpg")),
+            Container(
+              transform: Matrix4.translationValues(0.0, -12.0, 0.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                    // height:400,
+                    child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/dolphin.png"),
+                      const Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: const Text(
+                          "Share your travel experience and gain trusted rocs from your friends",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                      // const TextField(
+                      //   decoration: InputDecoration(
+                      //       border: OutlineInputBorder(
+                      //           borderRadius:
+                      //               BorderRadius.all(Radius.circular(8.0)))),
+                      // ),
+
+                      Container(
+                        width: 290,
+                        // height: MediaQuery.of(context).size.height * 0.06,
+                        // margin: const EdgeInsets.symmetric(
+                        //     vertical: 2, horizontal: 2),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text(
+                              "Login with phone number",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                              side: BorderSide(width: 2, color: Colors.blue)),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        //   width: MediaQuery.of(context).size.width * 0.9,
+                        //   height: MediaQuery.of(context).size.height * 0.06,
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 2, horizontal: 2),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "Login with phone Facebook",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(22.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "Dont have an account?",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              "Log In",
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                  // height:400,
-                  child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Image.asset("assets/dolphin.png"),
-                    Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: const Text(
-                        "Share your travel experience and gain trusted rocs from your friends",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ),
-                    // const TextField(
-                    //   decoration: InputDecoration(
-                    //       border: OutlineInputBorder(
-                    //           borderRadius:
-                    //               BorderRadius.all(Radius.circular(8.0)))),
-                    // ),
-
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Login with phone number",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                            side: BorderSide(width: 2, color: Colors.blue)),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: MediaQuery.of(context).size.height * 0.06,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Login with phone Facebook",
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(22.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Text(
-                            "Dont have an account?",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            "Log In",
-                            style: TextStyle(color: Colors.blue, fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
